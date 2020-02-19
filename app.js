17d9e7b9cfa5a8a54ac73dc0fc0ea8966d9d5c98
zhao ziyi <zhaoziy@oregonstate.edu>
2020-02-19 10:00:04 -0800

    got data from hard code data

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
-
+const data = require('./data/geo.js');
 const app = express();
-
+const request = require('superagent');
 app.get('/location', (req, res) => {
+    const cityData = data.results[0];
     res.json({
-        some:'json'
+        formatted_query : cityData.formatted_address,
+        latitude: cityData.geometry.location.lat,
+        longitude:cityData.geometry.location.lng,
+    });
+});
+app.get('*', (request, response) => {
+    response.json({
+        onNo: '404'
     });
 });
 
